2a55a32e803de3df8df0984d3e226ca4e653f7cb
BinaryLeo <binaryleo@pop-os.localdomain>
2022-10-30 01:29:27 -0300

    feat: add pulse animation

--- src/components/ActionButton/index.tsx
+++ src/components/ActionButton/index.tsx
@@ -1,10 +1,46 @@
-import { IonButton } from "@ionic/react";
-import React from "react";
-
+import { createAnimation, IonButton, IonIcon } from "@ionic/react";
+import { closeOutline, heart } from "ionicons/icons";
+import React, { useRef } from "react";
+import { pulseKeyFrames } from "../../constants";
+import "./style.css";
 export const ActionButtons: React.FC = () => {
+  const unmatchButtonRef = useRef<HTMLDivElement>(null);
+  const matchButtonRef = useRef<HTMLDivElement>(null);
+
+  const playUnmatchButtonAnimation = () => {
+    if (unmatchButtonRef.current != null) {
+      const animation = createAnimation()
+        .addElement(unmatchButtonRef.current)
+        .duration(1000)
+        .keyframes(pulseKeyFrames);
+      animation.play();
+    }
+  };
+  const playmatchButtonAnimation = () => {
+    if (matchButtonRef.current != null) {
+      const animation = createAnimation()
+        .addElement(matchButtonRef.current)
+        .duration(1000)
+        .keyframes(pulseKeyFrames);
+      animation.play();
+    }
+  };
   return (
     <div className="action-buttons-container">
-      <IonButton>Just a test</IonButton>
+      <div ref={unmatchButtonRef}>
+        <IonButton
+          onClick={playUnmatchButtonAnimation}
+          className="unmatch"
+          fill="solid"
+        >
+          <IonIcon icon={closeOutline}></IonIcon>
+        </IonButton>
+      </div>
+      <div ref={matchButtonRef}>
+        <IonButton onClick={playmatchButtonAnimation} className="match" fill="solid">
+          <IonIcon icon={heart}></IonIcon>
+        </IonButton>
+      </div>
     </div>
   );
 };
---
+++ src/constants/index.ts
@@ -0,0 +1,7 @@
+export const pulseKeyFrames = [
+  { offset: 0, transform: "scale(1)" },
+  { offset: 0.3, transform: "scale(1.1)" },
+  { offset: 0.5, transform: "scale(1)" },
+  { offset: 0.7, transform: "scale(1.2)" },
+  { offset: 1, transform: "scale(1)" },
+];
