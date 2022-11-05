ccb118840f0b3868d836225011d215ff7994604e
BinaryLeo <leomouracss@fastmail.com>
2022-11-04 23:51:20 -0300

    refactor: action button props

--- src/components/ActionButton/index.tsx
+++ src/components/ActionButton/index.tsx
@@ -1,61 +1,38 @@
 import { CreateAnimation, IonButton, IonIcon } from "@ionic/react";
-import { closeOutline, heart } from "ionicons/icons";
-import React, { useRef } from "react";
+import React from "react";
 import { pulseKeyFrames } from "../../constants";
 import "./style.css";
+
 interface ContainerProps {
-  animateUnmatchButton: boolean;
-  animateMatchButton: boolean;
+  startAnimateButton: boolean;
+  stopAnimateButton: boolean;
+  matchAnimationRef:any;
+  handleButtonRef:()=>void;
+  iconName:string;
+  className: "unmatch"|"match";
 }
 export const ActionButtons: React.FC<ContainerProps> = (props) => {
-  const unmatchAnimationRef = useRef<CreateAnimation>(null);
-  const matchAnimationRef = useRef<CreateAnimation>(null);
-
-  const playUnmatchButtonAnimation = () => {
-    if (unmatchAnimationRef.current !== null) {
-      unmatchAnimationRef.current.animation.play();
-    }
-  };
-  const playMatchButtonAnimation = () => {
-    if (matchAnimationRef.current !== null) {
-      matchAnimationRef.current.animation.play();
-    }
-  };
+ 
   return (
-    <div className="action-buttons-container">
+    <div className="action-buttons">
       <CreateAnimation
-        ref={unmatchAnimationRef}
+        ref={props.matchAnimationRef}
         duration={1000}
         iterations={1}
         keyframes={pulseKeyFrames}
-        play={props.animateUnmatchButton}
-        stop={!props.animateUnmatchButton}
+        play={props.startAnimateButton}
+        stop={props.stopAnimateButton}
       >
         <IonButton
-          onClick={playUnmatchButtonAnimation}
-          className="unmatch"
+          onClick={props.handleButtonRef}
+          className={props.className}
           fill="solid"
         >
-          <IonIcon icon={closeOutline}></IonIcon>
+          <IonIcon icon={props.iconName}></IonIcon>
         </IonButton>
       </CreateAnimation>
 
-      <CreateAnimation
-        ref={matchAnimationRef}
-        duration={1000}
-        iterations={1}
-        keyframes={pulseKeyFrames}
-        play={props.animateMatchButton}
-        stop={!props.animateMatchButton}
-      >
-        <IonButton
-          onClick={playMatchButtonAnimation}
-          className="match"
-          fill="solid"
-        >
-          <IonIcon icon={heart}></IonIcon>
-        </IonButton>
-      </CreateAnimation>
+     
     </div>
   );
 };
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,20 +1,32 @@
 import {
+  CreateAnimation,
   IonContent,
   IonHeader,
   IonPage,
   IonTitle,
   IonToolbar,
 } from "@ionic/react";
-import { useState } from "react";
+import { useRef, useState } from "react";
 import { ActionButtons } from "../components/ActionButton"; //* Pulse effect buttons
 import { ProfileCard } from "../components/ProfileCard"; //* Swipe cards
-import {profiles} from "../constants" //* Data provider
+import { profiles } from "../constants"; //* Data provider
 import "./Home.css";
 
+import { closeOutline, refresh } from "ionicons/icons";
 export const Home: React.FC = () => {
   const [animateUnmatchButton, setAnimateUnmatchButton] = useState(true);
   const [animateMatchButton, setAnimateMatchButton] = useState(true);
-  
+  const unmatchAnimationRef = useRef<CreateAnimation>(null);
+  const matchAnimationRef = useRef<CreateAnimation>(null);
+
+  const playUnmatchButtonAnimation = () =>
+    unmatchAnimationRef.current !== null &&
+    unmatchAnimationRef.current.animation.play();
+
+  const playMatchButtonAnimation = () =>
+    matchAnimationRef.current !== null &&
+    matchAnimationRef.current.animation.play();
+
   return (
     <IonPage>
       <IonHeader>
@@ -28,12 +40,24 @@ export const Home: React.FC = () => {
             <ProfileCard {...profile} key={profile.id} />
           ))}
         </div>
-        <ActionButtons
-          {...{
-            animateMatchButton,
-            animateUnmatchButton,
-          }}
-        />
+        <div className="action-buttons-container">
+          <ActionButtons
+            iconName={closeOutline}
+            startAnimateButton={animateMatchButton}
+            stopAnimateButton={!animateMatchButton}
+            matchAnimationRef={matchAnimationRef}
+            handleButtonRef={playMatchButtonAnimation}
+            className="match"
+          />
+          <ActionButtons
+            iconName={refresh}
+            startAnimateButton={animateUnmatchButton}
+            stopAnimateButton={!animateUnmatchButton}
+            matchAnimationRef={unmatchAnimationRef}
+            handleButtonRef={playUnmatchButtonAnimation}
+            className="unmatch"
+          />
+        </div>
       </IonContent>
     </IonPage>
   );
